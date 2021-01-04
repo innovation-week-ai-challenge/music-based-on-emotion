ce01b41c063be54f845ba1612b27de9b27f6543e
Tim-Van-den-Borre <72195069+Tim-Van-den-Borre@users.noreply.github.com>
2021-01-04 15:48:28 +0100

    Get a specific track

--- Play.py
+++ Play.py
@@ -4,15 +4,30 @@ import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
 from dotenv import load_dotenv
 
+# Spotify developer authentication. Consulted on 04/01/2021.
+
+# https://morioh.com/p/31b8a607b2b0 
+# https://developer.spotify.com/documentation/web-playback-sdk/quick-start/
+# https://spotipy.readthedocs.io/en/2.16.1/
+# https://developer.spotify.com/console/get-search-item/?q=tania+bowra&type=artist
+
 # Get the environment variables.
 load_dotenv()
 
 # Set the variables from the .env file.
 Client_ID = os.getenv('CLIENT_ID')
-Client_Secret = os.getenv('CLIENT_Secret')
+Client_Secret = os.getenv('Client_SECRET')
 
-# Authenticate on spotify developer (App)
-client_credentials_manager = SpotifyClientCredentials(client_id=Client_ID, client_secret=Client_Secret)
-sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
+SearchValue = 'Lose%20Yourself'
 
-print(sp)
+try:
+    # Authenticate on spotify developer (App)
+    client_credentials_manager = SpotifyClientCredentials(client_id=Client_ID, client_secret=Client_Secret)
+    sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
+    print('Authenticated!')
+except:
+    print('Could not authenticate, please check the authentication keys!')
+
+track_results = sp.search(q=SearchValue, type='track', limit=1,offset=1)
+
+print(track_results)
