04ff5a4859f561188dc48c8f0fc9a990bdd2cb5b
Tim-Van-den-Borre <72195069+Tim-Van-den-Borre@users.noreply.github.com>
2021-01-05 17:18:31 +0100

    Sequences for the emotions.

--- client/index.js
+++ client/index.js
@@ -1,9 +1,177 @@
 const improvCheckpoint =
-  'https://storage.googleapis.com/magentadata/js/checkpoints/music_rnn/chord_pitches_improv'
-const improvRNN = new mm.MusicRNN(improvCheckpoint)
+  "https://storage.googleapis.com/magentadata/js/checkpoints/music_rnn/chord_pitches_improv";
+const improvRNN = new mm.MusicRNN(improvCheckpoint);
 
-const synth = new Tone.Synth().toMaster()
-const { midi, Note } = Tonal
+const synth = new Tone.Synth().toMaster();
+const { midi, Note } = Tonal;
+
+/*
+    Chords. Consulted on 05/01/2021
+    https://www.edmprod.com/different-chord-types/
+    http://www.piano-keyboard-guide.com/piano-chords.html
+*/
+
+Happy = [
+  "Db5",
+  "G7",
+  "G5",
+  "Db6",
+  "F8",
+  "C5",
+  "Ab5",
+  "Db5",
+  "F8",
+  "G4",
+  "Db5",
+  "F8",
+];
+Sad = [
+  "Gb1",
+  "Ab3",
+  "G4",
+  "Ab2",
+  "F4",
+  "Eb2",
+  "Ab3",
+  "Gb4",
+  "Ab3",
+  "G4",
+  "Ab4",
+  "F3",
+];
+Angry = [
+  "Ab2",
+  "Ab1",
+  "Ab3",
+  "Gb2",
+  "C5",
+  "Eb7",
+  "Ab4",
+  "Gb2",
+  "Ab9",
+  "G6",
+  "Ab2",
+  "F3",
+];
+Confused = [
+  "Ab2",
+  "Ab1",
+  "Ab3",
+  "Gb2",
+  "F3",
+  "Eb7",
+  "Gb1",
+  "C5",
+  "G4",
+  "Ab2",
+  "F4",
+  "Eb2",
+];
+Disgusted = [
+  "F9",
+  "Gb2",
+  "F3",
+  "Eb7",
+  "Ab3",
+  "Gb4",
+  "Ab3",
+  "G4",
+  "G4",
+  "Ab2",
+  "F4",
+  "Eb2",
+];
+Surprised = [
+  "Ab4",
+  "Ab5",
+  "Ab4",
+  "Ab5",
+  "C5",
+  "Ab5",
+  "Ab4",
+  "Ab5",
+  "Ab4",
+  "Ab5",
+  "Ab4",
+  "Ab5",
+];
+Calm = [
+  "Ab4",
+  "F3",
+  "Ab4",
+  "C5",
+  "F3",
+  "Ab4",
+  "Ab4",
+  "CDF",
+  "Ab4",
+  "Ab4",
+  "F3",
+  "Ab4",
+];
+Unknown = [
+  "F3",
+  "F3",
+  "C5",
+  "F3",
+  "F3",
+  "C5",
+  "F3",
+  "F3",
+  "F3",
+  "C5",
+  "F3",
+  "F3",
+];
+Fear = [
+  "Bm",
+  "Bbm",
+  "Gb7",
+  "F7",
+  "Ab",
+  "Ab7",
+  "G7",
+  "Gb7",
+  "C5",
+  "Bb7",
+  "Eb7",
+  "AM7",
+];
+
+// Api call
+//response = fetch...
+
+switch (response) {
+  case (respose = "Happy"):
+    Melody = Happy;
+    break;
+  case (respose = "Sad"):
+    Melody = Sad;
+    break;
+  case (respose = "Angry"):
+    Melody = Angry;
+    break;
+  case (respose = "Confused"):
+    Melody = Confused;
+    break;
+  case (respose = "Disgusted"):
+    Melody = Disgusted;
+    break;
+  case (respose = "Surprised"):
+    Melody = Surprised;
+    break;
+  case (respose = "Calm"):
+    Melody = Calm;
+    break;
+  case (respose = "Unknown"):
+    Melody = Unknown;
+    break;
+  case (respose = "Fear"):
+    Melody = Fear;
+    break;
+  default:
+    Melody = Happy;
+}
 
 const sequence = {
   ticksPerQuarter: 220,
@@ -12,72 +180,59 @@ const sequence = {
     {
       time: 0,
       numerator: 4,
-      denominator: 4
-    }
+      denominator: 4,
+    },
   ],
   tempos: [
     {
       time: 0,
-      qpm: 120
-    }
+      qpm: 120,
+    },
   ],
   notes: [
-    { pitch: midi('Gb4'), startTime: 0, endTime: 1 },
-    { pitch: midi('F4'), startTime: 1, endTime: 3.5 },
-    { pitch: midi('Ab4'), startTime: 3.5, endTime: 4 },
-    { pitch: midi('C5'), startTime: 4, endTime: 4.5 },
-    { pitch: midi('Eb5'), startTime: 4.5, endTime: 5 },
-    { pitch: midi('Gb5'), startTime: 5, endTime: 6 },
-    { pitch: midi('F5'), startTime: 6, endTime: 7 },
-    { pitch: midi('E5'), startTime: 7, endTime: 8 },
-    { pitch: midi('Eb5'), startTime: 8, endTime: 8.5 },
-    { pitch: midi('C5'), startTime: 8.5, endTime: 9 },
-    { pitch: midi('G4'), startTime: 9, endTime: 11.5 },
-    { pitch: midi('F4'), startTime: 11.5, endTime: 12 },
-    { pitch: midi('Ab4'), startTime: 12, endTime: 12.5 },
-    { pitch: midi('C5'), startTime: 12.5, endTime: 13 },
-    { pitch: midi('Eb5'), startTime: 13, endTime: 14 },
-    { pitch: midi('D5'), startTime: 14, endTime: 15 },
-    { pitch: midi('Db5'), startTime: 15, endTime: 16 },
-    { pitch: midi('C5'), startTime: 16, endTime: 16.5 },
-    { pitch: midi('F5'), startTime: 16.5, endTime: 17 },
-    { pitch: midi('F4'), startTime: 17, endTime: 19.5 },
-    { pitch: midi('G4'), startTime: 19.5, endTime: 20 },
-    { pitch: midi('Ab4'), startTime: 20, endTime: 20.5 },
-    { pitch: midi('C5'), startTime: 20.5, endTime: 21 },
-    { pitch: midi('Eb5'), startTime: 21, endTime: 21.5 },
-    { pitch: midi('C5'), startTime: 21.5, endTime: 22 },
-    { pitch: midi('Eb5'), startTime: 22, endTime: 22.5 },
-    { pitch: midi('C5'), startTime: 22.5, endTime: 24.5 },
-    { pitch: midi('Eb5'), startTime: 24.5, endTime: 25.5 },
-    { pitch: midi('G4'), startTime: 25.5, endTime: 28.5 }
-  ]
-}
+    { pitch: midi("Gb4"), startTime: 0, endTime: 1 },
+    { pitch: midi("F4"), startTime: 1, endTime: 3.5 },
+    { pitch: midi("Ab4"), startTime: 3.5, endTime: 4 },
+    { pitch: midi("C5"), startTime: 4, endTime: 4.5 },
+    { pitch: midi("Eb5"), startTime: 4.5, endTime: 5 },
+    { pitch: midi("Gb5"), startTime: 5, endTime: 6 },
+    { pitch: midi("F5"), startTime: 6, endTime: 7 },
+    { pitch: midi("E5"), startTime: 7, endTime: 8 },
+    { pitch: midi("Eb5"), startTime: 8, endTime: 8.5 },
+    { pitch: midi("C5"), startTime: 8.5, endTime: 9 },
+    { pitch: midi("G4"), startTime: 9, endTime: 11.5 },
+    { pitch: midi("F4"), startTime: 11.5, endTime: 12 },
+    { pitch: midi("Ab4"), startTime: 12, endTime: 12.5 },
+    { pitch: midi("C5"), startTime: 12.5, endTime: 13 },
+    { pitch: midi("Eb5"), startTime: 13, endTime: 14 },
+    { pitch: midi("D5"), startTime: 14, endTime: 15 },
+    { pitch: midi("Db5"), startTime: 15, endTime: 16 },
+    { pitch: midi("C5"), startTime: 16, endTime: 16.5 },
+    { pitch: midi("F5"), startTime: 16.5, endTime: 17 },
+    { pitch: midi("F4"), startTime: 17, endTime: 19.5 },
+    { pitch: midi("G4"), startTime: 19.5, endTime: 20 },
+    { pitch: midi("Ab4"), startTime: 20, endTime: 20.5 },
+    { pitch: midi("C5"), startTime: 20.5, endTime: 21 },
+    { pitch: midi("Eb5"), startTime: 21, endTime: 21.5 },
+    { pitch: midi("C5"), startTime: 21.5, endTime: 22 },
+    { pitch: midi("Eb5"), startTime: 22, endTime: 22.5 },
+    { pitch: midi("C5"), startTime: 22.5, endTime: 24.5 },
+    { pitch: midi("Eb5"), startTime: 24.5, endTime: 25.5 },
+    { pitch: midi("G4"), startTime: 25.5, endTime: 28.5 },
+  ],
+};
 
-const quantizedSequence = mm.sequences.quantizeNoteSequence(sequence, 1)
+const quantizedSequence = mm.sequences.quantizeNoteSequence(sequence, 1);
 
 const startProgram = async () => {
   try {
-    await improvRNN.initialize()
+    await improvRNN.initialize();
     let improvisedMelody = await improvRNN.continueSequence(
       quantizedSequence,
       60,
       1.1,
-      [
-        'Bm',
-        'Bbm',
-        'Gb7',
-        'F7',
-        'Ab',
-        'Ab7',
-        'G7',
-        'Gb7',
-        'F7',
-        'Bb7',
-        'Eb7',
-        'AM7'
-      ]
-    )
+      Melody
+    );
 
     const playOriginalMelody = () => {
       sequence.notes.forEach((note) => {
@@ -85,9 +240,9 @@ const startProgram = async () => {
           Note.fromMidi(note.pitch),
           note.endTime - note.startTime,
           note.startTime
-        )
-      })
-    }
+        );
+      });
+    };
 
     const playGeneratedMelody = () => {
       improvisedMelody.notes.forEach((note) => {
@@ -95,21 +250,21 @@ const startProgram = async () => {
           Note.fromMidi(note.pitch),
           note.quantizedEndStep - note.quantizedStartStep,
           note.quantizedStartStep
-        )
-      })
-    }
+        );
+      });
+    };
 
-    const originalMelodyButton = document.getElementById('original')
-    const generatedMelodyButton = document.getElementById('generated')
+    const originalMelodyButton = document.getElementById("original");
+    const generatedMelodyButton = document.getElementById("generated");
     originalMelodyButton.onclick = () => {
-      playOriginalMelody()
-    }
+      playOriginalMelody();
+    };
     generatedMelodyButton.onclick = () => {
-      playGeneratedMelody()
-    }
+      playGeneratedMelody();
+    };
   } catch (error) {
-    console.error(error)
+    console.error(error);
   }
-}
+};
 
-startProgram()
+startProgram();
