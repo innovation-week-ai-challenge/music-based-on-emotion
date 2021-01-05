94c118647adfb98614e1e2af7828ce254da17031
Jarno Bogaert <jarnobogaert9@gmail.com>
2021-01-05 17:12:27 +0100

    Feat: api with route to get emotion out of portrait

---
+++ api/recognition.py
@@ -0,0 +1,23 @@
+import boto3
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+class RecognizeEmotion:
+  def __init__(self):
+    super().__init__()
+    access_key = os.getenv("AWS_ACCESS_KEY")
+    secret_key = os.getenv("AWS_SECRET_KEY")
+
+    self.client = boto3.client('rekognition', aws_access_key_id=access_key, aws_secret_access_key=secret_key)
+  
+  def get_emotion(self, image_bytes):
+    response = self.client.detect_faces(Image={'Bytes': image_bytes}, Attributes=['ALL'])
+
+    face_details = response.get('FaceDetails')
+
+    if not face_details:
+      return 'UNKNOWN'
+
+    emotions = face_details[0].get('Emotions')
+    return emotions[0].get('Type')
